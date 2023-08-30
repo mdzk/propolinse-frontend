aecd23c35729d5ef6bdf3a85318f68f337892ab7
Muhammad Dzaky <mdzaky07@gmail.com>
2023-08-31 04:58:18 +0700

    edit faq layout

--- src/pages/product/Faq.jsx
+++ src/pages/product/Faq.jsx
@@ -11,22 +11,22 @@ const Faq = () => {
       </div>
 
       <div className="container">
-        <div className="row">
-          <div className="col-md-6 text-center d-flex justify-content-center">
-            <img src="../../../public/assets/images/faq/free.png" alt="" />
-            <div>
-              <b>Free Shipping</b>
-              <p>Fee Shipping for orders two botle 600ml</p>
+        <div className="row d-flex align-items-center py-5">
+          <div className="col-md-6 text-center">
+            <div className="d-flex justify-content-center">
+              <img src="../../../public/assets/images/faq/free.png" alt="" />
             </div>
+            <b>Free Shipping</b>
+            <p>Fee Shipping for orders two botle 600ml</p>
           </div>
-          <div className="col-md-6">
 
+          <div className="col-md-6">
             <div className="accordion accordion-rounded" id="accordion-1">
-              <h2 className="card-title">
+              <p className="card-title">
                 <a className="collapsed" role="button" data-toggle="collapse" href="#collapse-4" aria-expanded="false" aria-controls="collapse-4" >
                   Kapan barang saya dikirim & mendapat resi pengiriman?
                 </a>
-              </h2>
+              </p>
               <div id="collapse-4" className="collapse" aria-labelledby="heading-4" data-parent="#accordion-1">
                 <div className="card-body">
                   Nullam malesuada erat ut turpis. Suspendisse urna
@@ -39,12 +39,162 @@ const Faq = () => {
                 </div>
               </div>
 
-              <h2 className="card-title">
+              <p className="card-title">
                 <a className="collapsed" role="button" data-toggle="collapse" href="#collapse-1" aria-expanded="false" aria-controls="collapse-1" >
                   Pengirimannya menggunakan apa? Dan berapa ongkos kirimnya?
                 </a>
-              </h2>
+              </p>
               <div id="collapse-1" className="collapse" aria-labelledby="heading-4" data-parent="#accordion-1">
+                <div className="card-body">
+                  Nullam malesuada erat ut turpis. Suspendisse urna
+                  nibh, viverra non, semper suscipit, posuere a, pede.
+                  Donec nec justo eget felis facilisis fermentum.Lorem
+                  ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Donec odio. Quisque volutpat mattis eros. Lorem ipsum
+                  dolor sit amet, consectetuer adipiscing elit. Donec
+                  odio. Quisque volutpat mattis eros.
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <hr />
+
+        <div className="row d-flex align-items-center py-5">
+          <div className="col-md-6 text-center">
+            <div className="d-flex justify-content-center">
+              <img src="../../../public/assets/images/faq/return.png" alt="" />
+            </div>
+            <b>Return</b>
+            <p>Within 30 days for an exchange</p>
+          </div>
+
+          <div className="col-md-6">
+            <div className="accordion accordion-rounded" id="accordion-2">
+              <p className="card-title">
+                <a className="collapsed" role="button" data-toggle="collapse" href="#collapse-2" aria-expanded="false" aria-controls="collapse-2" >
+                  Kapan barang saya dikirim & mendapat resi pengiriman?
+                </a>
+              </p>
+              <div id="collapse-2" className="collapse" aria-labelledby="heading-4" data-parent="#accordion-2">
+                <div className="card-body">
+                  Nullam malesuada erat ut turpis. Suspendisse urna
+                  nibh, viverra non, semper suscipit, posuere a, pede.
+                  Donec nec justo eget felis facilisis fermentum.Lorem
+                  ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Donec odio. Quisque volutpat mattis eros. Lorem ipsum
+                  dolor sit amet, consectetuer adipiscing elit. Donec
+                  odio. Quisque volutpat mattis eros.
+                </div>
+              </div>
+
+              <p className="card-title">
+                <a className="collapsed" role="button" data-toggle="collapse" href="#collapse-3" aria-expanded="false" aria-controls="collapse-3" >
+                  Pengirimannya menggunakan apa? Dan berapa ongkos kirimnya?
+                </a>
+              </p>
+              <div id="collapse-3" className="collapse" aria-labelledby="heading-4" data-parent="#accordion-2">
+                <div className="card-body">
+                  Nullam malesuada erat ut turpis. Suspendisse urna
+                  nibh, viverra non, semper suscipit, posuere a, pede.
+                  Donec nec justo eget felis facilisis fermentum.Lorem
+                  ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Donec odio. Quisque volutpat mattis eros. Lorem ipsum
+                  dolor sit amet, consectetuer adipiscing elit. Donec
+                  odio. Quisque volutpat mattis eros.
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <hr />
+
+        <div className="row d-flex align-items-center py-5">
+          <div className="col-md-6 text-center">
+            <div className="d-flex justify-content-center">
+              <img src="../../../public/assets/images/faq/support.png" alt="" />
+            </div>
+            <b>Online Support</b>
+            <p>Within 30 days for an exchange</p>
+          </div>
+
+          <div className="col-md-6">
+            <div className="accordion accordion-rounded" id="accordion-3">
+              <p className="card-title">
+                <a className="collapsed" role="button" data-toggle="collapse" href="#collapse-5" aria-expanded="false" aria-controls="collapse-5" >
+                  Kapan barang saya dikirim & mendapat resi pengiriman?
+                </a>
+              </p>
+              <div id="collapse-5" className="collapse" aria-labelledby="heading-4" data-parent="#accordion-3">
+                <div className="card-body">
+                  Nullam malesuada erat ut turpis. Suspendisse urna
+                  nibh, viverra non, semper suscipit, posuere a, pede.
+                  Donec nec justo eget felis facilisis fermentum.Lorem
+                  ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Donec odio. Quisque volutpat mattis eros. Lorem ipsum
+                  dolor sit amet, consectetuer adipiscing elit. Donec
+                  odio. Quisque volutpat mattis eros.
+                </div>
+              </div>
+
+              <p className="card-title">
+                <a className="collapsed" role="button" data-toggle="collapse" href="#collapse-6" aria-expanded="false" aria-controls="collapse-6" >
+                  Pengirimannya menggunakan apa? Dan berapa ongkos kirimnya?
+                </a>
+              </p>
+              <div id="collapse-6" className="collapse" aria-labelledby="heading-4" data-parent="#accordion-3">
+                <div className="card-body">
+                  Nullam malesuada erat ut turpis. Suspendisse urna
+                  nibh, viverra non, semper suscipit, posuere a, pede.
+                  Donec nec justo eget felis facilisis fermentum.Lorem
+                  ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Donec odio. Quisque volutpat mattis eros. Lorem ipsum
+                  dolor sit amet, consectetuer adipiscing elit. Donec
+                  odio. Quisque volutpat mattis eros.
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <hr />
+
+        <div className="row d-flex align-items-center py-5">
+          <div className="col-md-6 text-center">
+            <div className="d-flex justify-content-center">
+              <img src="../../../public/assets/images/faq/payment.png" alt="" />
+            </div>
+            <b>Flexible Payment</b>
+            <p>Within 30 days for an exchange</p>
+          </div>
+
+          <div className="col-md-6">
+            <div className="accordion accordion-rounded" id="accordion-4">
+              <p className="card-title">
+                <a className="collapsed" role="button" data-toggle="collapse" href="#collapse-7" aria-expanded="false" aria-controls="collapse-7" >
+                  Kapan barang saya dikirim & mendapat resi pengiriman?
+                </a>
+              </p>
+              <div id="collapse-7" className="collapse" aria-labelledby="heading-4" data-parent="#accordion-4">
+                <div className="card-body">
+                  Nullam malesuada erat ut turpis. Suspendisse urna
+                  nibh, viverra non, semper suscipit, posuere a, pede.
+                  Donec nec justo eget felis facilisis fermentum.Lorem
+                  ipsum dolor sit amet, consectetuer adipiscing elit.
+                  Donec odio. Quisque volutpat mattis eros. Lorem ipsum
+                  dolor sit amet, consectetuer adipiscing elit. Donec
+                  odio. Quisque volutpat mattis eros.
+                </div>
+              </div>
+
+              <p className="card-title">
+                <a className="collapsed" role="button" data-toggle="collapse" href="#collapse-8" aria-expanded="false" aria-controls="collapse-8" >
+                  Pengirimannya menggunakan apa? Dan berapa ongkos kirimnya?
+                </a>
+              </p>
+              <div id="collapse-8" className="collapse" aria-labelledby="heading-4" data-parent="#accordion-4">
                 <div className="card-body">
                   Nullam malesuada erat ut turpis. Suspendisse urna
                   nibh, viverra non, semper suscipit, posuere a, pede.
